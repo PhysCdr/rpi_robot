0a20c2313a0c35974feec74c12e313fa475f21ae
Dmitriy <d.zusin@gmail.com>
2020-08-23 12:53:37 -0600

    updated GPIO pins. Client connect address changed to remote

--- tests/test_diff_drive_control.py
+++ tests/test_diff_drive_control.py
@@ -2,10 +2,11 @@ import communication.socket_comm as sc
 import socket
 import threading
 from control.diff_drive import DiffDrive
+from control.odometry_control import OdoControl
 
 client = sc.Socket()
-port = 8008
-client.connect(socket.gethostname(), port)
+port = 8001
+client.connect('192.168.0.9', port)
 listener_thread = threading.Thread(target=client.recv_stream)
 odometry_control = OdoControl(run_method=client.get_msg_data)
 odometry_control_thread = threading.Thread(target=odometry_control.receive_control, args=(client,))
@@ -13,8 +14,8 @@ listener_thread.daemon = True
 odometry_control_thread.daemon = True
 listener_thread.start()
 odometry_control_thread.start()
-left = (4,14)
-right = (5,15)
+left = (7,8)
+right = (9,10)
 diff_drive = DiffDrive(left, right)
 
 while  True:
